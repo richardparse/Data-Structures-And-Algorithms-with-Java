9882be2848358f30852018b624b097037b1a0b58
Richard Parse <richard.parse@hotmail.com>
2019-07-22 00:48:00 +0200

    StackDemo.

--- JavaDataStructuresAlgorithms/src/dataStructures/BitSetDemo.java
+++ JavaDataStructuresAlgorithms/src/dataStructures/BitSetDemo.java
@@ -8,15 +8,31 @@ public class BitSetDemo {
 		BitSet bits1 = new BitSet(16);
 		BitSet bits2 = new BitSet(16);
 
-		
-		//set some bits
-		for(int i=0; i<16; i++) {
-			if((i%2) == 0) bits1.set(i);
-			if((i%5) != 0) bits2.set(i);
+		// set some bits
+		for (int i = 0; i < 16; i++) {
+			if ((i % 2) == 0)
+				bits1.set(i);
+			if ((i % 5) != 0)
+				bits2.set(i);
 		}
-		System.out.println("Initial pattern in bits");
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		System.out.println("Initial pattern in bits1: ");
+		System.out.println(bits1);
+		System.out.println("\nInitial pattern in bits2: ");
+		System.out.println(bits2);
+
+		// AND bits
+		bits2.and(bits1);
+		System.out.println("\nbits2 AND bits1: ");
+		System.out.println(bits2);
+
+		// OR bits
+		bits2.or(bits1);
+		System.out.println("\nbits2 OR bits1: ");
+		System.out.println(bits2);
+
+		// XOR bits
+		bits2.xor(bits1);
+		System.out.println("\nbits2 XOR bits1: ");
+		System.out.println(bits2);
 	}
 }
